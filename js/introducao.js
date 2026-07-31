6a6fc80001a1d3a83a81dd914f8b06733d9610a0
Maria Paula <mpfonseca@prodesp.fazenda.sp.gov.br>
2026-07-30 21:30:23 -0300

    Aula de Javascript - Parte 1

---
+++ js/introducao.js
@@ -0,0 +1,34 @@
+console.log("Olá, mundo!");
+
+
+var salarioBruto = 2000;
+var salarioLiquido = salarioBruto - (salarioBruto * 0.2);
+var nomeFuncionario = "Paula";
+
+var mensagem = "O funcionário " + nomeFuncionario + " tem um salário líquido de R$ " + salarioLiquido + ". O salário bruto é de R$ " + salarioBruto + ".";
+var mensagem2 = `O funcionário ${nomeFuncionario} tem um salário líquido de R$ ${salarioLiquido}. O salário bruto é de R$ ${salarioBruto}.`;
+
+/* console.log(mensagem);
+console.log(mensagem2);
+
+
+var salario = prompt("Digite o salário do funcionário:");
+console.log('O salário digitado foi: R$ ' + salario + '.');
+
+var novoSalario;    
+var mensagemNovoSalario
+if (salario < 1000) {
+    novoSalario = Number(salario) + 100;
+    mensagemNovoSalario = `O salário é inferior a R$ 1000. O novo salário será R$ ${novoSalario}.`;
+}else{
+    novoSalario = salario;
+    mensagemNovoSalario = `O salário é igual ou superior a R$ 1000. O salário fica o mesmo R$ ${novoSalario}.`;
+}
+
+    console.log(mensagemNovoSalario); */
+
+/* var confirmar = confirm("Tem certeza que deseja fechar?");
+console.log(confirmar);
+ */
+
+alert ("Olá mundo!!");
